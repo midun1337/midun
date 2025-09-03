c2e87fab880df138158ff41d7306996f810105b3
midun1337 <dhanicoy7@gmail.com>
2025-09-04 02:06:58 +0700

    Update app.js

--- whoami/app.js
+++ whoami/app.js
@@ -1,43 +1,49 @@
- // --- fallback IP lookup ---
-    function fallbackIP() {
-      fetch("https://ipapi.co/json/")
-        .then(res => res.json())
-        .then(data => {
-          document.getElementById("info").innerHTML = `
-            <b>Who?</b><br>
-            IP: ${data.ip}<br>
-            Negara: ${data.country_name}<br>
-            Provinsi: ${data.region}<br>
-            Kota: ${data.city}<br>
-            ISP: ${data.org}<br>
-            Lat, Long: ${data.latitude}, ${data.longitude}<br>
-          `;
-        })
-        .catch(() => {
-          document.getElementById("info").innerHTML = "Gagal mengambil lokasi (IP).";
-        });
+    function tampilkanData(ipData, gpsData = null) {
+      let output = `
+        <b>IP Publik:</b> ${ipData.ip}<br>
+        <b>Negara:</b> ${ipData.country_name}<br>
+        <b>Provinsi:</b> ${ipData.region}<br>
+        <b>Kota:</b> ${ipData.city}<br>
+        <b>ISP:</b> ${ipData.org}<br>
+      `;
+
+      if (gpsData) {
+        // Jika GPS diizinkan
+        output += `
+          <b>Lokasi Akurat (GPS):</b><br>
+          Lat, Long: ${gpsData.coords.latitude}, ${gpsData.coords.longitude}<br>
+          Akurasi: ${gpsData.coords.accuracy} meter<br>
+        `;
+      } else {
+        // Jika GPS ditolak → fallback ke ipapi lat/long
+        output += `
+          <b>Lokasi (IP Based):</b><br>
+          Lat, Long: ${ipData.latitude}, ${ipData.longitude}<br>
+        `;
+      }
+
+      document.getElementById("info").innerHTML = output;
     }
 
-    // --- coba GPS dulu ---
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          let loc = pos.coords.latitude + ", " + pos.coords.longitude;
-          document.getElementById("info").innerHTML = `
-            <b>I'm is </b><br>
-            Loc: ${loc}<br>
-            Acc: ${pos.coords.accuracy} meter<br>
-          `;
-        },
-        (err) => {
-          document.getElementById("info").innerHTML = `
-            Gagal GPS (${err.message})<br>
-            Menggunakan fallback IP...<br><br>
-          `;
-          fallbackIP();
-        },
-        { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 } // opsi GPS
-      );
-    } else {
-      fallbackIP();
-    }
+    // --- Ambil IP & info negara/prov/kota/ISP ---
+    fetch("https://ipapi.co/json/")
+      .then(res => res.json())
+      .then(ipData => {
+        // --- Coba GPS dulu ---
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (pos) => {
+              tampilkanData(ipData, pos);
+            },
+            (err) => {
+              // Kalau GPS ditolak → pakai IP-based saja
+              tampilkanData(ipData);
+            }
+          );
+        } else {
+          tampilkanData(ipData);
+        }
+      })
+      .catch(err => {
+        document.getElementById("info").innerHTML = "Gagal ambil data lokasi!";
+      });
