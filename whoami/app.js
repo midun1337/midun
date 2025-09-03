719fc43bababe4722fcb85c9c4b5d87469494d1a
midun1337 <dhanicoy7@gmail.com>
2025-09-04 02:11:43 +0700

    Update app.js

--- whoami/app.js
+++ whoami/app.js
@@ -1,42 +1,35 @@
     function tampilkanData(ipData, gpsData = null) {
       let output = `
-        <b>IP Publik:</b> ${ipData.ip}<br>
-        <b>Negara:</b> ${ipData.country_name}<br>
-        <b>Provinsi:</b> ${ipData.region}<br>
-        <b>Kota:</b> ${ipData.city}<br>
-        <b>ISP:</b> ${ipData.org}<br>
+        <b>IP :</b> ${ipData.ip}<br>
+        <b>Negara :</b> ${ipData.country_name}<br>
+        <b>Provinsi :</b> ${ipData.region}<br>
+        <b>Kota :</b> ${ipData.city}<br>
+        <b>ISP :</b> ${ipData.org}<br>
       `;
 
       if (gpsData) {
-        // Jika GPS diizinkan
         output += `
-          <b>Lokasi Akurat (GPS):</b><br>
-          Lat, Long: ${gpsData.coords.latitude}, ${gpsData.coords.longitude}<br>
-          Akurasi: ${gpsData.coords.accuracy} meter<br>
+          <b>Loc :</b> ${gpsData.coords.latitude}, ${gpsData.coords.longitude}<br>
+          <b>Akurasi :</b> ${gpsData.coords.accuracy} meter<br>
         `;
       } else {
-        // Jika GPS ditolak → fallback ke ipapi lat/long
         output += `
-          <b>Lokasi (IP Based):</b><br>
-          Lat, Long: ${ipData.latitude}, ${ipData.longitude}<br>
+          <b>Loc :</b> ${ipData.latitude}, ${ipData.longitude}<br>
         `;
       }
 
       document.getElementById("info").innerHTML = output;
     }
 
-    // --- Ambil IP & info negara/prov/kota/ISP ---
     fetch("https://ipapi.co/json/")
       .then(res => res.json())
       .then(ipData => {
-        // --- Coba GPS dulu ---
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
             (pos) => {
               tampilkanData(ipData, pos);
             },
             (err) => {
-              // Kalau GPS ditolak → pakai IP-based saja
               tampilkanData(ipData);
             }
           );
